6661c1ca4ca72193b8dedabc8143cc20feb6ca2a
Gor <you@example.com>
2022-12-20 10:55:54 +0400

    last changes in web

--- src/ui/sign-in/sign-in.tsx
+++ src/ui/sign-in/sign-in.tsx
@@ -1,35 +1,84 @@
 import { FC } from 'react';
 
-// import { useSignIn } from 'ui/sign-in/hook/use-sign-in';
+import { Link } from 'react-router-dom';
 
+import { signInFormConfig } from 'ui/sign-in/sign-in-form-config';
+
+// import { useSignIn } from 'ui/sign-in/hook/use-sign-in';
+//
 const SignIn: FC = () => {
-  // const formFieldsRef = useRef<IRefAuthFormFields>({
-  //   userName: null,
-  //   password: null,
-  // });
+  //   const formFieldsRef = useRef<IRefAuthFormFields>({
+  //     userName: null,
+  //     password: null,
+  //   });
   //
-  // const [signIn, errors] = useSignIn();
+  //   const [signIn, errors] = useSignIn();
   //
-  // const [userData, setUserData] = useState<IAuthFormFields>({
-  //   userName: '',
-  //   password: '',
-  // });
+  //   const [userData, setUserData] = useState<IAuthFormFields>({
+  //     userName: '',
+  //     password: '',
+  //   });
   //
-  // const signInFormChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-  //   const { name, value } = e.target;
+  //   const signInFormChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+  //     const { name, value } = e.target;
   //
-  //   setUserData({ ...userData, ...{ [name]: value } });
-  // };
+  //     setUserData({ ...userData, ...{ [name]: value } });
+  //   };
   //
-  // const handleKeyDown = (event: { key: string }) => {
-  //   if (event.key === 'Enter') {
-  //     signIn(userData);
-  //   }
-  // };
+  //   const handleKeyDown = (event: { key: string }) => {
+  //     if (event.key === 'Enter') {
+  //       signIn(userData);
+  //     }
+  //   };
 
   return (
-    <section className="sign-in-wrapper">
-      <div className="container">sign in</div>
+    <section className="signIn-wrapper">
+      <div className="container">
+        <h4 style={{ color: 'white', marginBottom: '15px', fontSize: '22px', textAlign: 'center' }}>Sign In</h4>
+        <form>
+          {signInFormConfig.map(item => {
+            const { key, label, type } = item;
+
+            return (
+              <label key={key} style={{ color: 'white' }}>
+                {label}
+                <input
+                  // ref={ref => (formFieldsRef.current[key] = ref)}
+                  key={key}
+                  name={key}
+                  // onChange={signInFormChangeHandler}
+                  // value={userData[key]}
+                  // label={label}
+                  // variant="outlined"
+                  className="input"
+                  // error={!!errors[key]}
+                  // helperText={errors[key]}
+                  type={type}
+                  // onKeyDown={handleKeyDown}
+                  // autoComplete={userData[key]}
+                  // InputProps={{
+                  //     endAdornment: rightIcon ? (
+                  //         <InputAdornment position="end">
+                  //             <Eye
+                  //                 makeVisible={value => {
+                  //                     if (formFieldsRef.current && formFieldsRef.current[key]) {
+                  //                         formFieldsRef.current[key]!.type = value ? 'text' : 'password';
+                  //                     }
+                  //                 }}
+                  //             />
+                  //         </InputAdornment>
+                  //     ) : null,
+                  // }}
+                />
+              </label>
+            );
+          })}
+        </form>
+        <Link className="forgot-password" to={'/forgot-password'}>
+          Forgot Password?
+        </Link>
+        <button className="submit-button">login</button>
+      </div>
     </section>
   );
 };
